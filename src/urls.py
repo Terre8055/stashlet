fb85e815721b7e4e84935704a4583b286c6e2b5e
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-10-24 21:53:03 +0000

    Add global urls file to src directory

---
+++ src/urls.py
@@ -0,0 +1,13 @@
+"""
+Module for storing GLOBAL URLs for all endpoints.
+"""
+
+GLOGIN = '/auth/login'
+GLOGOUT= '/auth/login'
+GREGISTER= '/auth/register'
+GFORGOT_PASSWORD= '/auth/forgot-password'
+GENTER_NEW_STRING= '/auth/enter-new-string'
+
+
+GHOME='/app/'
+GHOMEPAGE='/'
