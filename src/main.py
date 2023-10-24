1a5954bb037b82054ca5857fdd425e72571c1392
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-10-24 21:54:10 +0000

    Main file to run program

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from bottle import Bottle, get
+from common.auth import auth
+from common.homepage import home
+from public.dashboard import dashboard
+
+
+root = Bottle()
+
+
+
+if __name__ == "__main__":
+    root.merge(home)
+    root.mount('/auth', auth)
+    root.mount('/app', dashboard)
+    root.run(host='localhost', port=8080, debug=True, reloader=True)
+
