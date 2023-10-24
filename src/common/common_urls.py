a93a0f898ccdd61d2c04225d703fc36e8d5cc1bb
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-10-24 21:53:41 +0000

    Add local urls in src

---
+++ src/common/common_urls.py
@@ -0,0 +1,11 @@
+"""
+Module for storing local URLs for authentication-related endpoints.
+"""
+
+__all__ = ['LOGIN', 'LOGOUT', 'REGISTER', 'FORGOT_PASSWORD', 'ENTER_NEW_STRING']
+
+LOGIN = '/login'
+LOGOUT = '/logout'
+REGISTER = '/register'
+FORGOT_PASSWORD = '/forgot-password'
+ENTER_NEW_STRING = '/enter-new-string'
---
+++ src/public/public_urls.py
@@ -0,0 +1,7 @@
+"""
+Module for storing local URLs for protected endpoints.
+"""
+
+__all__ = ['HOME',]
+
+HOME = '/'
