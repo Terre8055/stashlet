482ddf54d7f3870788eabc3a2c167d7330edcce1
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-11-05 18:32:41 +0000

    Add close-account link to comnmon urls

--- src/common/common_urls.py
+++ src/common/common_urls.py
@@ -2,10 +2,11 @@
 Module for storing local URLs for authentication-related endpoints.
 """
 
-__all__ = ['LOGIN', 'LOGOUT', 'REGISTER', 'FORGOT_PASSWORD', 'ENTER_NEW_STRING']
+__all__ = ['LOGIN', 'LOGOUT', 'REGISTER', 'FORGOT_PASSWORD', 'ENTER_NEW_STRING', 'CLOSE_ACCOUNT']
 
 LOGIN = '/login'
 LOGOUT = '/logout'
 REGISTER = '/register'
 FORGOT_PASSWORD = '/forgot-password'
 ENTER_NEW_STRING = '/enter-new-string'
+CLOSE_ACCOUNT = '/close-account'
