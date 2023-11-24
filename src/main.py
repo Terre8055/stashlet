914ffb692b03a7c73563e413178a4244ad02477c
Michael Appiah Dankwah <117749434+Terre8055@users.noreply.github.com>
2023-11-24 16:00:59 +0000

    Run app on all network interface

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
-from bottle import Bottle, get, static_file
+from bottle import Bottle, get, static_file, TEMPLATE_PATH
 from common.auth import auth
 from common.homepage import home
 from public.dashboard import dashboard
 
-
+TEMPLATE_PATH.append('/app/src/views')
 root = Bottle()
 
 
@@ -13,5 +13,5 @@ if __name__ == "__main__":
     root.merge(home)
     root.mount('/auth', auth)
     root.mount('/app', dashboard)
-    root.run(host='localhost', port=8080, debug=True, reloader=True)
+    root.run(host='0.0.0.0', port=8080, debug=True, reloader=True)
 
